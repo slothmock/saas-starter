8f61eab7c1b4c79bfad5db128601cf3b586ddac1
Jordan "sloth" Mock <j.mock_@outlook.com>
2025-06-03 14:50:32 +0100

    Add better dark mode support and hover underline for links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -81,11 +81,13 @@ function UserMenu() {
 
 function Header() {
   return (
-    <header className="border-b border-gray-200">
+    <header className="border-b border-gray-200 dark:border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
           <TrashIcon className="h-6 w-6 text-orange-500" />
-          <span className="ml-1 text-lg font-semibold text-gray-900">PembsWasteSMS</span>
+          <span className="ml-1 text-lg font-semibold text-gray-900 dark:text-gray-100">
+            PembsWasteSMS
+          </span>
         </Link>
         <div className="ml-2 flex items-center space-x-4">
           <Suspense fallback={<div className="h-9" />}>
